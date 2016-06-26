b0cd88d32730dc94c82af301715a8f1980652c65
letehaha <letehaha@gmail.com>
2016-06-26 17:17:21 +0300

    update

--- dist/js/common.js
+++ dist/js/common.js
@@ -2,12 +2,26 @@ $(function() {
 	$('.main-nav_elem a').smoothScroll();
 });
 
-var slides = document.querySelectorAll('.testimonial_slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,7000);
+////////// first slider
 
-function nextSlide() {
-    slides[currentSlide].className = 'testimonial_slide';
-    currentSlide = (currentSlide+1)%slides.length;
-    slides[currentSlide].className = 'testimonial_slide showing';
+var slides_first = document.querySelectorAll('.testimonial_slide_first');
+var currentSlide_first = 0;
+var slideInterval_first = setInterval(nextSlide_first,5000);
+
+function nextSlide_first() {
+    slides_first[currentSlide_first].className = 'testimonial_slide_first';
+    currentSlide_first = (currentSlide_first+1)%slides_first.length;
+    slides_first[currentSlide_first].className = 'testimonial_slide_first showing';
+}
+
+////////// second slider
+
+var slides_second = document.querySelectorAll('.testimonial_slide_second');
+var currentSlide_second = 0;
+var slideInterval_second = setInterval(nextSlide_second,5000);
+
+function nextSlide_second() {
+    slides_second[currentSlide_second].className = 'testimonial_slide_second';
+    currentSlide_second = (currentSlide_second+1)%slides_second.length;
+    slides_second[currentSlide_second].className = 'testimonial_slide_second showing';
 }
