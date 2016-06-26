414ef3a74302a35895d061042b6486c93c665099
letehaha <letehaha@gmail.com>
2016-06-26 22:57:52 +0300

    update

--- dist/js/common.js
+++ dist/js/common.js
@@ -25,3 +25,46 @@ function nextSlide_second() {
     currentSlide_second = (currentSlide_second+1)%slides_second.length;
     slides_second[currentSlide_second].className = 'testimonial_slide_second showing';
 }
+
+//////// google maps api
+
+function initMap() {
+
+  // Specify features and elements to define styles.
+  var styleArray = [
+    {
+      featureType: "all",
+      stylers: [
+       { saturation: -80 }
+      ]
+    },{
+      featureType: "road.arterial",
+      elementType: "geometry",
+      stylers: [
+        { hue: "#00ffee" },
+        { saturation: 10 }
+      ]
+    },{
+      featureType: "poi.business",
+      elementType: "labels",
+      stylers: [
+        { visibility: "off" }
+      ]
+    }
+  ];
+  // Create a map object and specify the DOM element for display.
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 33.773718, lng: -118.206581},
+    scrollwheel: false,
+    styles: styleArray,
+    zoom: 12,
+    disableDefaultUI: true,
+    zoomControl: true
+  });
+  var image = '../img/map_marker.png';
+  var marker = new google.maps.Marker({
+    position: {lat: 33.772006, lng: -118.209327},
+    map: map,
+    icon: image
+  });
+};
